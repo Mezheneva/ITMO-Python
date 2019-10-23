5eb5852281d6bda3759d6b68ccd7e7a195014634
Irina <irm98@mail.ru>
2019-10-23 21:44:27 +0300

    23.10 контрольная работа задача 17

---
+++ test/game.py
@@ -0,0 +1,27 @@
+import random
+
+str = ""
+
+while str != "exit":
+
+    maximum_number = int(input('Enter maximum number> '))
+
+    computer_number = random.randint(1, maximum_number)
+
+    number_of_attempts = int(input('Enter number of attempts> '))
+
+    for count_of_attempts in range(0, number_of_attempts):
+
+        user_number = int(input('Enter number> '))
+
+        if user_number > computer_number:
+            print('The number is greater than the hidden')
+        elif user_number < computer_number:
+            print('The number is less than the hidden')
+        else:
+            print('You guessed')
+            break
+    else:
+        print('Attempts ended')
+
+    str = input('The game is over. To exit write \"exit\" ')
